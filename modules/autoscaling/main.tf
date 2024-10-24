682ef29391b4c255b9d910fcd379999796fe8f66
HoaiNampro <namn2591@gmail.com>
2024-10-24 16:06:29 +0700

    update at load balancer

--- modules/autoscaling/main.tf
+++ modules/autoscaling/main.tf
@@ -1,4 +1,70 @@
+# Tạo Application Load Balancer
+resource "aws_lb" "alb" {
+  name               = "${var.project}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [var.sg.lb]
+  subnets            = var.vpc.public_subnets
+}
 
+# Tạo target group cho webv1
+resource "aws_lb_target_group" "webv1" {
+  name        = "ver1"
+  port        = 8000
+  protocol    = "HTTP"
+  vpc_id      = var.vpc.vpc_id
+  target_type = "instance"
+
+  stickiness {
+    type            = "lb_cookie"
+    cookie_duration = 86400  # Cookie tồn tại trong 1 ngày
+  }
+}
+
+# Tạo target group cho webv2
+resource "aws_lb_target_group" "webv2" {
+  name        = "ver2"
+  port        = 8000
+  protocol    = "HTTP"
+  vpc_id      = var.vpc.vpc_id
+  target_type = "instance"
+
+  stickiness {
+    type            = "lb_cookie"
+    cookie_duration = 86400  # Cookie tồn tại trong 1 ngày
+  }
+}
+
+# Tạo listener cho ALB với sticky session và cân bằng tải giữa webv1 và webv2
+resource "aws_lb_listener" "alb_listener" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "forward"
+
+    forward {
+      target_group {
+        arn    = aws_lb_target_group.webv1.arn
+        weight = 1  # Tỷ lệ cho webv1
+      }
+
+      target_group {
+        arn    = aws_lb_target_group.webv2.arn
+        weight = 1  # Tỷ lệ cho webv2
+      }
+
+      # Bật stickiness cho listener action (group stickiness)
+      stickiness {
+        enabled  = true
+        duration = 86400  # Cookie tồn tại trong 1 ngày
+      }
+    }
+  }
+}
+
+# Cấu hình Launch Template cho webv1
 resource "aws_launch_template" "webv1" {
   name_prefix   = "web1-"
   image_id      = "ami-0c653ba233fb5b919"
@@ -11,12 +77,9 @@ resource "aws_launch_template" "webv1" {
     db_user     = var.db_config.user
     db_password = var.db_config.password
   }))
-  # network_interfaces {
-  #   associate_public_ip_address = true
-  #   security_groups             = [var.sg.web]
-  # }
 }
 
+# Cấu hình Launch Template cho webv2
 resource "aws_launch_template" "webv2" {
   name_prefix   = "web2-"
   image_id      = "ami-0c653ba233fb5b919"
@@ -29,79 +92,34 @@ resource "aws_launch_template" "webv2" {
     db_user     = var.db_config.user
     db_password = var.db_config.password
   }))
-  # network_interfaces {
-  #   associate_public_ip_address = true
-  #    security_groups             = [var.sg.web]
-  # }
 }
 
-
-
-
-module "alb" {
-  source             = "terraform-aws-modules/alb/aws"
-  version            = "~> 6.0"
-  name               = var.project
-  load_balancer_type = "application"
-  vpc_id             = var.vpc.vpc_id
-  subnets            = var.vpc.public_subnets
-  security_groups    = [var.sg.lb]
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
-      action = {
-        type = "forward"
-        target_group_tuple = [
-          {
-            target_group_index = 0
-            weight             = 3  # Tỷ lệ 75%
-          },
-          {
-            target_group_index = 1
-            weight             = 1  # Tỷ lệ 25%
-          }
-        ]
-      }
-    }
-  ]
-  target_groups = [
-    {
-      name_prefix      = "ver1",
-      backend_protocol = "HTTP",
-      backend_port     = 8000
-      target_type      = "instance"
-    },
-    {
-      name_prefix      = "ver2",
-      backend_protocol = "HTTP",
-      backend_port     = 8000
-      target_type      = "instance"
-    }
-  ]
-}
-
+# Cấu hình Auto Scaling Group cho webv1
 resource "aws_autoscaling_group" "webv1" {
   name                = "${var.project}-webv1-asg"
   min_size            = 1
   max_size            = 3
   vpc_zone_identifier = var.vpc.private_subnets
-  target_group_arns   = module.alb.target_group_arns
+  target_group_arns   = [aws_lb_target_group.webv1.arn]
+  
   launch_template {
     id      = aws_launch_template.webv1.id
     version = aws_launch_template.webv1.latest_version
   }
 }
 
+# Cấu hình Auto Scaling Group cho webv2
 resource "aws_autoscaling_group" "webv2" {
   name                = "${var.project}-webv2-asg"
   min_size            = 1
   max_size            = 3
   vpc_zone_identifier = var.vpc.private_subnets
-  target_group_arns   = module.alb.target_group_arns
+  target_group_arns   = [aws_lb_target_group.webv2.arn]
+  
   launch_template {
     id      = aws_launch_template.webv2.id
     version = aws_launch_template.webv2.latest_version
   }
 }
+
+
--- modules/autoscaling/outputs.tf
+++ modules/autoscaling/outputs.tf
@@ -1,3 +1,3 @@
 output "lb_dns" {
-  value = module.alb.lb_dns_name
+  value = aws_lb.alb.dns_name
 }
